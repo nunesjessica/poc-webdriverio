54ea38cccf9fdf58ca4c865e92b0c06271a704e6
Jessica Nunes <nunesbsi@gmail.com>
2018-11-01 10:20:46 -0300

    alterando o import e refatorando as funções para arrow fuctions

--- features/step-definitions/novo-usuario.steps.js
+++ features/step-definitions/novo-usuario.steps.js
@@ -1,21 +1,20 @@
-import  expect  from 'chai';
+const expect = require('chai').expect;
+const { Given, When, Then } = require('cucumber');
 
-import { Given, When, Then } from 'cucumber';
+Given('que estou na página de criação de usuário', () => 
+   browser.url('https://automacaocombatista.herokuapp.com/users/new')
+);
 
-Given('que estou na página de criação de usuário', function (){
-    return browser.url("https://automacaocombatista.herokuapp.com/users/new")
-});
-
-When('insiro todas as informações obrigatórias para criação', function(){
+When('insiro todas as informações obrigatórias para criação', () => {
     $('#user_name').setValue('test');
     $('#user_lastname').setValue('user');
     $('#user_email').setValue('user@test.com');
 });
 
-When('clico no botão para submissão', function(){
+When('clico no botão para submissão', () => {
     $('//*[@name="commit"]').click();
 });
 
-Then('vejo o usuário criado com sucesso', function(){
+Then('vejo o usuário criado com sucesso', () => {
     expect($('#notice').getText()).to.equal('Usuário Criado com sucesso');
 });
